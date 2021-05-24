e8114e88ac9282282ce6c016b92bf3c0f5391f02
Daniel Bruenker <Daniel.Bruenker@gmx.de>
2021-05-24 17:08:00 +0200

    Fix bug where alert message was not visible

--- frontend/src/alert/components/customAlert.tsx
+++ frontend/src/alert/components/customAlert.tsx
@@ -11,12 +11,11 @@ const CustomAlert: React.FC = () => {
   const { alerts } = useTypedSelector((state) => state.alert);
 
   const toast = useRef<Toast | null>(null);
-  const [alert, setAlert] = useState({ type: "success", message: "" });
 
   useEffect(() => {
     if (alerts.length > 0) {
       if (alerts[alerts.length - 1].visible) {
-        setAlert(alerts[alerts.length - 1]);
+        let alert = alerts[alerts.length - 1];
         if(!toast.current){return;}
         toast.current.show({severity: alert.type, summary: alert.message, life: 5000});
         dispatch(alertActions.hideAlert({ index: alerts.length - 1 }));
@@ -25,7 +24,7 @@ const CustomAlert: React.FC = () => {
   }, [alerts, dispatch]);
 
   return (
-    <Toast ref={toast}  position="bottom-center" />
+    <Toast ref={toast}  position="top-center" />
   );
 
 };
